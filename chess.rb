ceb82d617b688521b169e6088a03b13976f83ea3
Paulius Neesme <Paulius Neesme>
2016-10-17 21:41:44 +0300

    modified chess

--- chess.rb
+++ chess.rb
@@ -1,13 +1,27 @@
-lines = File.open("data.txt", 'r').read.split("\n").drop(1)
+lines = File.open('duom.txt', 'r').read.split("\n").drop(1)
+
 boards = []
-result = []
+
 lines.each do |line|
-  boards << line.split.map(&:to_i)
-end
-result << boards.transpose.map {|x| x.reduce(:+)}
-complect = [8, 2, 2, 2, 1, 1]
-complect.each_with_index do |elem, index|
-  result[index] = result[index] / elem
+ boards << line.split.map(&:to_i)
 end
 
-puts result.min
+result = Array.new(6, 0)
+boards.each do |board|
+  board.size.times do |index|
+    result[index] += board[index]
+  end
+end
+
+complete_boards =[
+ result[0] / 8,
+ result[1] / 2,
+ result[2] / 2,
+ result[3] / 2,
+ result[4] / 1,
+ result[5] / 1
+]
+
+puts complete_boards.min
+
+#puts boards.transpose.map {|x| x.reduce(:+)}
